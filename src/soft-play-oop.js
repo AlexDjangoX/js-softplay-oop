5f094abcc9c185d28fba671221052c47ff00061a
AlexDjangoX <alexmonk17@gmail.com>
2022-03-11 17:22:15 +0100

    passed al tests

--- src/soft-play-oop.js
+++ src/soft-play-oop.js
@@ -1,6 +1,38 @@
-// TODO: Write your class in this file
+class SoftPlay {
+  constructor () {
+    this.adults = 0
+    this.children = 0
+  }
 
-// TODO: Change undefined to the name of your class
+  occupancy () {
+    return {
+      adults: this.adults,
+      children: this.children
+    }
+  }
+
+  enter (numAdults, numChildren) {
+    if (numChildren > numAdults) { return false } else {
+      this.adults += numAdults
+      this.children += numChildren
+      return true
+    }
+  }
+
+  leave (numAdults, numChildren) {
+    const remainCountAdults = this.adults - numAdults
+    const remainCountChildren = this.children - numChildren
+    if (numChildren > numAdults) { return false }
+    else if (remainCountChildren > remainCountAdults) { return false }
+    else if (numAdults > this.adults || numChildren > this.children) { return false }
+    else {
+      this.adults -= numAdults
+      this.children -= numChildren
+      return true
+    }
+  }
+}
+
 module.exports = {
-  SoftPlay: undefined
+  SoftPlay: SoftPlay
 }
